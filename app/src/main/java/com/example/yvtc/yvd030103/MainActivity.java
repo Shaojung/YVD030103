e56440fe818391ef5291d054be779ac108fe4e4c
Shaojung <shaojung@gmail.com>
2017-03-01 15:05:32 +0800

    startActivityForResult

--- app/src/main/java/com/example/yvtc/yvd030103/MainActivity.java
+++ app/src/main/java/com/example/yvtc/yvd030103/MainActivity.java
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -32,5 +33,18 @@ public class MainActivity extends AppCompatActivity {
 
         it.putExtras(b);
         startActivity(it);
+    }
+
+    public void click3(View v)
+    {
+        Intent it = new Intent(MainActivity.this, FourthActivity.class);
+        startActivityForResult(it, 321);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        TextView tv5 = (TextView) findViewById(R.id.textView5);
+        tv5.setText(data.getStringExtra("ans"));
     }
 }
